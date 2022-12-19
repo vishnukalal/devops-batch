e569e6ba7f6742db997178ecdb7140d6d1e1ae98
vishnukalal <kalalvishnu0612@gmail.com>
2022-12-19 12:35:10 -0500

    first python session

---
+++ main.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+a = 2
+b = 6.0
+c = "vishnu"
+
+print(a, b, c)
+
+l = [1,2,3,4,8,4,6,3]
+t = (12,3,8,205,12,8)
+s = {1,3,5,7,3,5,9}
+d = {1:1, 2:2, 2:4, 3:5, 3:7, 4:5, 4:7}
+
+print(l)
+print(t)
+print(s)
+print(d)
+
+
+l = [1,2,5,8,9,12,15,85,42,65,98,8,5,12]
+
+print(l)
+print(l.index(85))
+print(l[5])
+print(l[8])
